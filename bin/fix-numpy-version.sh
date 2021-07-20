609e1d95592fdcb8fcd7446a6ecf7f86601e2569
Sindre Nistad <snis@equinor.com>
2021-07-20 14:43:09 +0200

    fix

--- bin/fix-numpy-version.sh
+++ bin/fix-numpy-version.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+name=$(python setup.py --fullname)
 minimum_supported_python="$(cat LOWEST_SUPPORTED_PYTHON_VERSION.txt)"
 minimum_supported_numpy=$(python bin/find_lowest_supported_numpy.py "$minimum_supported_python")
 
 # Update pyproject.toml
 sed -i -E "s/\"numpy==(.*)\",/\"numpy>=$minimum_supported_numpy\",/g" "pyproject.toml"
+mv "dist/${name}.tar.gz" wheelhouse
