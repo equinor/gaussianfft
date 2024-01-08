4b3531a2d9c4ad59fd8148b6d326e04405f808dd
Sindre Nistad <snis@equinor.com>
2024-01-08 12:01:44 +0100

    fix: Ensure file is available

--- cmake/utilities.cmake
+++ cmake/utilities.cmake
@@ -1,3 +1,11 @@
+if (NOT EXISTS ${CMAKE_BINARY_DIR}/bin)
+    file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
+endif ()
+file(
+        COPY_FILE ${CMAKE_SOURCE_DIR}/bin/find_dependants.py
+        ${CMAKE_BINARY_DIR}/bin/find_dependants.py
+        ONLY_IF_DIFFERENT
+)
 function(dependants output_variables)
     set(ENV{PYTHONPATH} ${CMAKE_BINARY_DIR})
     execute_process(
