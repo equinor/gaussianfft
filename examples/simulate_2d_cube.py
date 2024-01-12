f7926c4d3f823f9627cdafefdb6c602072bfd8fe
Sindre Nistad <snis@equinor.com>
2024-01-12 12:30:20 +0100

    fix: Make example consistent with current API of `gaussianfft`

--- examples/simulate_2d_cube.py
+++ examples/simulate_2d_cube.py
@@ -4,13 +4,7 @@ import gaussianfft as grf
 
 grf.seed(123)
 
-# Option A:
-variogram1 = grf.ExponentialVariogram(xrange=1000, yrange=1000)
-# Option B (tror denne er å foretrekke da det er naturlig å kunne legge navn i en liste hvis en ønsker iterasjon):
-# variogram2 = grf.variogram.create('exponential', xrange=1000, yrange=1000)
-variogram2 = grf.variogram('exponential', xrange=1000, yrange=1000)
-# Option C (Hvis vi ikke ønsker hierarki av Variogram-klassen)
-variogram3 = grf.Variogram(xrange=1000, yrange=1000, correlation=lambda x, y: x-y)
+variogram = grf.variogram('exponential', main_range=1000, perp_range=1000)
 
 nx = 100
 ny = 100
@@ -18,19 +12,15 @@ x = np.linspace(1000.0, 2000.0, nx)
 y = np.linspace(-3000.0, -1000.0, nx)
 
 # Option A
-field1 = grf.simulate2d(variogram1, nx=nx, ny=nx, dx=1000.0/nx, dy=2000.0/ny)
-# Option B
-field2 = grf.simulate2d(variogram1, xlen=100, ylen=100, dx=10.0, dy=10.0)
-# Option C
-field3 = grf.simulate2d(variogram1, xlen=100, ylen=100, nx=10, ny=10)
+field = grf.simulate(variogram, nx=nx, ny=nx, dx=1000.0 / nx, dy=2000.0 / ny)
 
 plt.subplot(211)
-plt.imshow(x, y, field1)
+plt.imshow(field.reshape((nx, ny)))
 plt.title('Simulate 2D Gaussian field')
 plt.colorbar()
 
 plt.subplot(212)
-plt.imshow(np.roll(field1, int(nx/2), axis=0))
+plt.imshow(np.roll(field.reshape((nx, ny)), int(nx / 2), axis=0))
 plt.title('The same field, shifted (should not be continuous)')
 plt.colorbar()
 plt.show()
