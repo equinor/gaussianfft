8f376ea186ad0b457fa35c4c28723af9bb618908
Sindre Nistad <snis@equinor.com>
2024-01-24 17:25:28 +0100

    feat: Preprocess source files to determine necessary dependencies
    This ensures files that are only included if `FFTW_DEBUG` is set, is cont compiled when they are not used
    
    This is necessary for #45, as it will _not_ download / compile Boost for production builds (e.g. on GitHub Actions)

--- bin/find_dependants.py
+++ bin/find_dependants.py
@@ -1,9 +1,29 @@
-import sys
+import argparse
 import utils
+import sysconfig
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser("find-dependencies")
+    parser.add_argument("--use-preprocessor", action="store_true", help="Whether to use the C++ preprocessor on the source files before finding their dependencies")
+    parser.add_argument("source_files", nargs="+", help="List of source files to look through")
+    parser.add_argument("--include-directories", help="List of directories to include while preprocessing the file(s), seperated by ';'")
+    return parser.parse_args()
 
 
 def run():
-    print(' '.join(utils.collect_sources(sys.argv[1:])))
+    args = parse_arguments()
+    include_directories = None
+    if args.include_directories:
+        include_directories = args.include_directories.split(";")
+        include_directories.append(f"{sysconfig.get_path('data')}/include")
+
+    print(' '.join(utils.collect_sources(
+        args.source_files,
+        use_preprocessor=args.use_preprocessor,
+        include_directories=include_directories,
+        ignore=include_directories,
+    )))
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,7 +1,48 @@
 import logging
+import os
 import re
+import subprocess
+import sysconfig
 from pathlib import Path
 from typing import Iterable, Optional, List
+
+
+def preprocess(file: Path, include_directories: Optional[List[str]] = None) -> Optional[List[str]]:
+    cxx = os.environ.get("CXX")
+    if cxx:
+        # GCC, and older versions of clang may fail if it cannot find references to the #include directives
+        # Additionally, the result will not be annotated with -E -dI
+        # Clang does not require it, though
+        if include_directories is None:
+            additional_include_directories = []
+        else:
+            additional_include_directories = [f"-I{directory}" for directory in include_directories]
+            additional_include_directories.append(f"-I{sysconfig.get_path('data')}/include")
+        cxx_flags = os.environ.get("CXXFLAGS") or ""
+        res = subprocess.run(" ".join([
+            cxx,
+            *cxx_flags.split(" "),
+            "-E",
+            "-dI",
+            *additional_include_directories,
+            str(file),
+        ]),
+            check=False,
+            capture_output=True,
+            shell=True,
+        )
+        if res.returncode > 0:
+            raise SystemError(res.stderr)
+        if b'-E -dI' in res.stdout:
+            return [
+                line.decode("utf-8")
+                for line in re.findall(b'^.*-E -dI.*$', res.stdout + res.stderr, re.MULTILINE)
+            ]
+        else:
+            return [
+                line.decode("utf-8")
+                for line in re.findall(b'^# *1 +.*$', res.stdout + res.stderr, re.MULTILINE)
+            ]
 
 
 def collect_sources(
@@ -9,6 +50,8 @@ def collect_sources(
         ignore: Optional[List[str]] = None,
         source_dir: str = 'src',
         use_absolute: bool = False,
+        use_preprocessor: bool = True,
+        include_directories: Optional[List[str]] = None,
 ) -> List[str]:
     if ignore is None:
         ignore = []
@@ -47,6 +90,9 @@ def collect_sources(
             if file.suffix in ['.cpp', '.c']:
                 _add_file(r'.h\1', source_regex)
 
+    include_pattern = re.compile(r'^ *# *include *[<"](?P<name>[a-z0-9/._]*)[">]', re.IGNORECASE)
+    define_pattern = re.compile(r'^ *# *define \w+ *(\\\n)? *[<"](?P<name>[\w/.]+)[>"]', re.IGNORECASE | re.MULTILINE)
+    completely_preprocessed_pattern = re.compile(r'^ *# *\d+ *[<"](?P<name>[\w/.]+)[>"]', re.IGNORECASE)
     while len(files_to_be_inspected) > 0:
 
         file = files_to_be_inspected.pop().resolve()
@@ -59,32 +105,35 @@ def collect_sources(
         if name in files:
             continue
 
-        include_pattern = re.compile(r'^ *# *include *[<"](?P<name>[a-z0-9/._]*)[">]', re.IGNORECASE)
-        define_pattern = re.compile(r'^ *# *define \w+ *(\\\n)? *[<"](?P<name>[\w/.]+)[>"]', re.IGNORECASE | re.MULTILINE)
-
+        with file.open(encoding='utf8') as f:
+            if use_preprocessor:
+                source_lines = preprocess(file, include_directories)
+            if not use_preprocessor or source_lines is None:
+                source_lines = f.readlines()
         try:
-            with file.open(encoding='utf8') as f:
-                previous_line = ''
-                for line in f.readlines():
-                    match = include_pattern.search(line)
-                    if not match:
-                        match = define_pattern.search(line)
-                    if not match:
-                        if not previous_line.endswith('\n'):
-                            previous_line += '\n'
-                        match = define_pattern.search(previous_line + line)
-                    if match:
-                        item = Path(match.group('name'))
-                        if (file.parent / item).is_file():
-                            add_if_necessary(file.parent / item)
-                        elif item.is_file():
-                            add_if_necessary(item)
-                        elif (src / item).is_file():
-                            add_if_necessary(src / item)
-                        else:
-                            pass
-                            # logging.info(file, item)
-                    previous_line = line
+            previous_line = ''
+            for line in source_lines:
+                match = include_pattern.search(line)
+                if not match:
+                    match = define_pattern.search(line)
+                if not match:
+                    if not previous_line.endswith('\n'):
+                        previous_line += '\n'
+                    match = define_pattern.search(previous_line + line)
+                if not match:
+                    match = completely_preprocessed_pattern.search(line)
+                if match:
+                    item = Path(match.group('name'))
+                    if (file.parent / item).is_file():
+                        add_if_necessary(file.parent / item)
+                    elif item.is_file():
+                        add_if_necessary(item)
+                    elif (src / item).is_file():
+                        add_if_necessary(src / item)
+                    else:
+                        pass
+                        # logging.info(file, item)
+                previous_line = line
         except UnicodeDecodeError:
             logging.info(f"'{name}' could not be opened / decoded as a text file. It's been ignored")
 
