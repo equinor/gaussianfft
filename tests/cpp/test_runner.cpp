3ca1d7579479c7cbc1fec7dcb2811b616825d07e
Feda Curic <feda.curic@gmail.com>
2026-02-18 09:34:04 +0100

    Run C++ tests
    Co-authored-by: Sindre Nistad <38072308+sindre-nistad@users.noreply.github.com>

---
+++ tests/cpp/test_runner.cpp
@@ -0,0 +1,6 @@
+// Main test runner for nrlib C++ unit tests using Boost.Test
+#define BOOST_TEST_MODULE NRLib Tests
+#include <boost/test/unit_test.hpp>
+
+// This file provides the main() function for Boost.Test.
+// Individual test files will be linked together to create the test executable.
